8fbbb96553e59aa6e0fa0d43c7e319cf2deb03bb
MiqueiasGomes06 <miqueiasgomes.2001@gmail.com>
2023-08-24 20:37:00 -0300

    base do app criacao do backgroundcolor e do botton

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"
+import { View, Text, StyleSheet, SafeAreaView, StatusBar, TouchableOpacity, FlatList} from "react-native";
+import {Ionicons} from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return(
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#171d31" barStyle="light-content"/>
+      
+      <View style={styles.content}>
+        <Text style={styles.title}> Minhas Tarefas </Text>
+      </View>
+
+    
+
+     
+     <TouchableOpacity style={styles.fab}>
+        <Ionicons name= "ios-add" size={35} color="#FFF"/>
+     </TouchableOpacity>
+
+
+
+      </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container:{
+      flex:1,
+      backgroundColor: '#171d31',
+    },
+    title:{
+      marginTop: 10,
+      paddingBottom: 10,
+      fontSize: 25,
+      textAlign: 'center',
+      color: '#FFF'
+    },
+    fab:{
+      position: 'absolute',
+      width: 60,
+      height: 60,
+      backgroundColor: '#0094FF',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 30,
+      right: 25,
+      bottom: 25,
+      elevation: 2,
+      zIndex: 9,
+      shadowColor: '#000',
+      shadowOpacity: 0.2,
+      shadowOffset:{
+        width:1,
+        height: 3,
+      }
+
+
+    }
 });
